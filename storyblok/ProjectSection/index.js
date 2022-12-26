7f70bb8c3073e58c92c7ff70ec0c074019bd5e29
Dan Spratling <dan@skyward.digital>
2022-12-26 18:11:20 +0000

    Staggers projects if odd

--- storyblok/ProjectSection/index.js
+++ storyblok/ProjectSection/index.js
@@ -7,32 +7,46 @@ const Projects = ({ blok }) => {
   const editable = storyblokEditable(blok);
   const { title, description, buttons, projects } = blok;
 
+  const projectsChunks = [
+    projects.slice(0, Math.ceil(projects.length / 2)), // array of items from index 0 -> half
+    projects.slice(Math.ceil(projects.length / 2)), // array of items from index half -> end
+  ];
+
   return (
     <section {...editable} className="py-12 md:py-20">
       <div className="container">
-        <div className="mx-auto max-w-5xl text-center">
-          <h2
-            {...editable}
-            className="mb-8 text-3xl font-normal leading-none tracking-tight text-gray-900 dark:text-gray-50 md:text-4xl lg:text-5xl"
-          >
-            {title}
-          </h2>
+        {title || description.content[0].content ? (
+          <div className="mx-auto mb-12 max-w-5xl text-center lg:mb-20">
+            <h2
+              {...editable}
+              className="mb-8 text-3xl font-normal leading-none tracking-tight text-gray-900 dark:text-gray-50 md:text-4xl lg:text-5xl"
+            >
+              {title}
+            </h2>
 
-          <RichText
-            {...editable}
-            data={description}
-            className="mb-12 text-lg font-light text-gray-700 dark:text-gray-200 md:text-xl lg:text-2xl"
-          />
-        </div>
+            <RichText
+              {...editable}
+              data={description}
+              className="mb-12 text-lg font-light text-gray-700 dark:text-gray-200 md:text-xl lg:text-2xl"
+            />
+          </div>
+        ) : null}
 
-        <div className="mx-auto my-12 grid max-w-7xl gap-x-12 gap-y-20 md:mt-24 md:mb-20 md:grid-cols-2">
-          {projects.map((project, index) => (
-            <ProjectCard key={index} project={project} />
+        <div className="mx-auto mb-12 flex max-w-7xl flex-wrap gap-16 md:flex-nowrap md:items-center lg:mb-20 lg:gap-24">
+          {projectsChunks.map((items, chunkIndex) => (
+            <div
+              key={chunkIndex}
+              className="grid w-full gap-16 md:w-1/2 md:gap-48"
+            >
+              {items.map((project, index) => (
+                <ProjectCard key={index} project={project} />
+              ))}
+            </div>
           ))}
         </div>
 
         {buttons.length ? (
-          <div className="flex flex-wrap justify-center gap-3">
+          <div className="mt-12 flex flex-wrap justify-center gap-3">
             {buttons.map((button, index) => (
               <Button key={index} link={button.link} style={button.style}>
                 {button.label}
@@ -61,7 +75,7 @@ const ProjectCard = ({ project }) => {
       />
 
       <div className="flex items-center justify-between gap-6">
-        <RichText data={title} className="text-xl" />
+        <p className="text-xl">{title}</p>
         <Link
           href={project.full_slug}
           className="btn btn-secondary btn-lg flex-min"
